6a51880c5e18d4bb88aa17d724aa0d510ca6068f
harleyhihew <harleyhihew@gmail.com>
2017-06-08 11:55:24 +0700

    แทน

---
+++ untitled1.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr  6 10:10:43 2017
+
+@author: Harleyhihew
+"""
+
+def CreateSLRTable(trans,state,Terms,nonTerms):
+    table = [[0 for x in range(len(Terms)+len(nonTerms)+1)] for y in range(len(state)+1)]
+    mixTnt = Terms+nonTerms
+    for s in state:
+        
+            
+        
