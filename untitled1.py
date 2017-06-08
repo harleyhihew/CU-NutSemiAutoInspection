1ecc5f6e6f7413aaeaed5339d5ea130537827bad
harleyhihew <harleyhihew@gmail.com>
2017-06-08 12:02:38 +0700

    ผิดๆ

--- untitled1.py
+++ untitled1.py
@@ -7,9 +7,4 @@ Created on Thu Apr  6 10:10:43 2017
 
 def CreateSLRTable(trans,state,Terms,nonTerms):
     table = [[0 for x in range(len(Terms)+len(nonTerms)+1)] for y in range(len(state)+1)]
-    mixTnt = Terms+nonTerms
-    for s in state:
-        
-        dsfsdkjsdjfsdf
-            
-        
+    mixTnt = Terms+nonTerms   
